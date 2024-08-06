cc491229fbe09fc0a707a79c607233d2d0d22f34
FFutagawa <ffutagawa@gmail.com>
2024-08-06 15:33:42 -0300

    Criado o template list.html e criada a classe create (POST)

--- src/main/java/br/com/treinaweb/twtodos/controllers/TodoController.java
+++ src/main/java/br/com/treinaweb/twtodos/controllers/TodoController.java
@@ -1,9 +1,11 @@
 package br.com.treinaweb.twtodos.controllers;
 
+import br.com.treinaweb.twtodos.models.Todo;
 import br.com.treinaweb.twtodos.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -28,4 +30,17 @@ public class TodoController {
 //     outra maneira desta maneira abaixo
 //        return new ModelAndView("todo/list", Map.of("todos", todoRepository.findAll()));
     }
+    @GetMapping("/create")
+    public ModelAndView create(){
+        return new ModelAndView("todo/form", Map.of("todo", new Todo()));
+    }
+
+    @PostMapping("/create")
+    public String create(Todo todo){
+        todoRepository.save(todo);
+        return "redirect:/";
+    }
 }
+
+
+
